e689357c6c58281afcf25996cec2bce930104650
Erik Gilling <konkers@konkers.net>
2019-10-01 22:33:44 -0700

    Make travis-build.sh cross platform.

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-./tools/linux/arduino-cli core update-index || exit 1
-./tools/linux/arduino-cli lib update-index || exit 1
-./tools/linux/arduino-cli core install SparkFun:apollo3 || exit 1
-./tools/linux/arduino-cli compile --fqbn SparkFun:apollo3:amap3nano || exit 1
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    PLATFORM="linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    PLATFORM="mac"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    PLATFORM="win"
+elif [[ "$OSTYPE" == "msys" ]]; then
+    PLATFORM="win"
+elif [[ "$OSTYPE" == "win32" ]]; then
+    PLATFORM="win"
+else
+    echo "Unknown platform $OSTYPE"
+    exit 1
+fi
+
+CLI=./tools/${PLATFORM}/arduino-cli
+
+${CLI} core update-index || exit 1
+${CLI} lib update-index || exit 1
+${CLI} core install SparkFun:apollo3 || exit 1
+${CLI} compile --fqbn SparkFun:apollo3:amap3nano || exit 1
