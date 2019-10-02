24872e7f1a3145b670100898dd294db966b2d430
Erik Gilling <konkers@konkers.net>
2019-10-01 22:31:04 -0700

    Add build and upload scripts.
    Adds cross platform support as well

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,7 +2,5 @@
 DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 source ${DIR}/common.sh
 
-${CLI} core update-index || exit 1
-${CLI} lib update-index || exit 1
-${CLI} core install SparkFun:apollo3 || exit 1
 ${CLI} compile --fqbn SparkFun:apollo3:amap3nano || exit 1
+${CLI} upload --fqbn SparkFun:apollo3:amap3nano --port=$1 || exit 1
